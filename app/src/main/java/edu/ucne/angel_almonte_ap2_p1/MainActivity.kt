23e5554c2a8cc04be59eb9aaa17fae25ff77cf0b
AngelParedes12 <angel_paredes@ucne.edu.do>
2026-02-04 19:32:49 -0400

    preparando el MainActivity.kt para el primer parcial

--- app/src/main/java/edu/ucne/angel_almonte_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/angel_almonte_ap2_p1/MainActivity.kt
@@ -3,33 +3,19 @@ package edu.ucne.angel_almonte_ap2_p1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.rememberDrawerState
-import androidx.navigation.compose.rememberNavController
+import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.angel_almonte_ap2_p1.ui.theme.Angel_Almonte_AP2_P1Theme
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setContent {
-            Angel_Almonte_AP2_P1Theme {
-                val navController = rememberNavController()
-                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-
-                DrawerMenu(
-                    drawerState = drawerState,
-                    navHostController = navController
-                ) {
-                    AppNavHost(
-                        navController = navController,
-                        modifier = Modifier.fillMaxSize()
-                    )
+    class MainActivity : ComponentActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            enableEdgeToEdge()
+            setContent {
                 }
             }
         }
     }
-}
