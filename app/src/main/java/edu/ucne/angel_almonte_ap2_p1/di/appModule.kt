c490a43abf3b36eaa1888c84b71d10b32f8ea830
AngelParedes12 <angel_paredes@ucne.edu.do>
2026-02-04 20:42:04 -0400

    configurando el appModule.kt para el primer parcial

--- app/src/main/java/edu/ucne/angel_almonte_ap2_p1/di/appModule.kt
+++ app/src/main/java/edu/ucne/angel_almonte_ap2_p1/di/appModule.kt
@@ -1,10 +1,35 @@
 package edu.ucne.angel_almonte_ap2_p1.di
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.angel_almonte_ap2_p1.data.local.dataBase.appDataBase
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object appModule {
+
+@Provides
+@Singleton
+fun  provideExamenDatabase(@ApplicationContext appContext: Context): appDataBase {
+    return Room.databaseBuilder(
+        appContext,
+        appDataBase::class.java,
+        "examen.db"
+    ).fallbackToDestructiveMigration().build()
+
+}
+    @Provides
+    fun provideBorraDao(appDataBase: appDataBase) = appDataBase.BorrarDao()
+
+    @Provides
+    @Singleton
+    fun provideBorraRepository(borraDao: BorrarDao) = BorraRepository(borraDao)
+
+
 }
