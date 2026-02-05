685fd013fd930edbe827752cfa39c3940aa7e880
AngelParedes12 <angel_paredes@ucne.edu.do>
2026-02-04 20:38:57 -0400

    preparando el ExamenNavHost.kt para el primer parcial

---
+++ app/src/main/java/edu/ucne/angel_almonte_ap2_p1/presentantion/navigation/ExamenNavHost.kt
@@ -0,0 +1,36 @@
+package edu.ucne.angel_almonte_ap2_p1.presentantion.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.BlendMode.Companion.Screen
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun ExamenNavHost(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.listScreen.route
+    ) {
+        composable<Screen.listScreen> {
+            ListScreen(
+                onAddTask = {
+                    navController.navigate(Screen.editScreen)
+                },
+                onEditTask = { id ->
+
+                }
+            )
+        }
+
+        composable<Screen.editScreen> {
+            EditScreen(
+                onBack = {
+                    navController.navigateUp()
+                }
+            )
+        }
+    }
+}
